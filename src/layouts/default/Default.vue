c097497bf540e75654926ab4b33206c0dc1ea771
Oumar Dicko <etu.programmeur@gmail.com>
2023-05-21 19:06:49 +0100

    adding page tree and component list pages

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,15 @@
-<template>
-  <v-app>
-    <default-bar />
-
-    <default-view />
-  </v-app>
-</template>
-
 <script lang="ts" setup>
   import DefaultBar from './AppBar.vue'
   import DefaultView from './View.vue'
+  import UiBar from './UiBar.vue';
 </script>
+
+<template>
+  <v-app>
+    <!-- <default-bar /> -->
+    <ui-bar />
+
+    <!-- <default-view /> -->
+  </v-app>
+</template>
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,39 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import { COMPONENT_LIST, PAGE_COMPONENT } from "./route-name";
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
-]
+    {
+        path: "/",
+        component: () => import("@/layouts/default/Default.vue"),
+        children: [
+            {
+                path: "",
+                name: PAGE_COMPONENT,
+                components: {
+                    builder: () =>
+                        import(
+                            /* webpackChunkName: "page-components" */ "@/components/page/page-components.vue"
+                        ),
+                },
+            },
+            {
+                path: "components",
+                name: COMPONENT_LIST,
+                components: {
+                    builder: () =>
+                        import(
+                            /* webpackChunkName: "page-components" */ "@/components/list/component-list.vue"
+                        ),
+                },
+            },
+        ],
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
